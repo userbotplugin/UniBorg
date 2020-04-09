5bce2d8e6fc2edefd9997ba607b8fbe46746949c
xxgameryt123 <63223801+xxgameryt123@users.noreply.github.com>
2020-04-09 07:43:38 +0200

    Update emojis.py

--- stdplugins/emojis.py
+++ stdplugins/emojis.py
@@ -9,22 +9,22 @@ import asyncio
 from uniborg.util import admin_cmd
 
 
-@borg.on(admin_cmd(pattern="emoji (.*)"))
+@borg.on(admin_cmd(pattern="spam (.*)"))
 async def _(event):
     if event.fwd_from:
         return
     animation_interval = 0.3
     animation_ttl = range(0, 16)
     input_str = event.pattern_match.group(1)
-    if input_str == "shrug":
-        await event.edit("¯\_(ツ)_/¯")
-    elif input_str == "apple":
-        await event.edit("\uF8FF")
-    elif input_str == ":/":
+    if input_str == "plugin":
+        await event.edit("@UserBotPlugin")
+    elif input_str == "whatsapp":
+        await event.edit("https://chat.whatsapp.com/K2QOz1RdpBO85AsGrkd6xL")
+    elif input_str == "youtube":
         await event.edit(input_str)
         animation_chars = [
-            ":\\",
-            ":/"
+            "XxGamerQSDProXx ElectroFazYT",
+            "XxGamerQSDProXx ElectroFazYT"
         ]
         for i in animation_ttl:
             await asyncio.sleep(animation_interval)
